e21b670fb6cbfec91156335bc1057c3a3bf954a5
fay wu <wru.fay@gmail.com>
2026-05-21 11:22:28 -0300

    write main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/api/noise")
+
+def root():
+    return {"message": "app is running"}
+
+@app.get("/")
+def root():
+    return {"message": "Noise app is running"}
+
+@app.get("/api/noise")
+def get_noise():
+    return {
+        "source": "wind_turbine_1",
+        "location": {"lat": 44.5, "lon": -63.5},
+        "decibels": 120
+    }
+
+# command to uvicorn main:app --reload
